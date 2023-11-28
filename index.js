82481267b90a674cc5af53bd630c857443bb47f2
Ranjeet Kumar Raj <121329638+RanjeetRaj444@users.noreply.github.com>
2023-11-28 10:08:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
-const { createServer } = require("node:http");
+const http = require("http");
 const { join } = require("node:path");
+
 const { Server } = require("socket.io");
 const path = require("path");
+
 const app = express();
-const server = createServer(app);
+const server = http.createServer(app);
 const io = new Server(server);
+
 app.use(express.static(path.resolve("./public")));
 app.get("/", (req, res) => {
 	res.sendFile("/public/index.html");
